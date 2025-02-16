4920577560643ded72d6c5004056eaffdd820989
luanxcastilho <luan.castilho@outlook.com.br>
2025-02-16 13:25:40 -0300

    Todas as repositories passam a utilizar JPA O banco utilizado passa a ser MySQL Nova API adicionada: usuarios-perfis

--- munchbox/src/main/java/br/com/fiap/munchbox/repositories/ProprietarioEnderecoRepository.java
+++ munchbox/src/main/java/br/com/fiap/munchbox/repositories/ProprietarioEnderecoRepository.java
@@ -1,11 +1,8 @@
 package br.com.fiap.munchbox.repositories;
 
-import br.com.fiap.munchbox.dtos.proprietarioendereco.ProprietarioEnderecoRequestDTO;
 import br.com.fiap.munchbox.entities.ProprietarioEndereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface ProprietarioEnderecoRepository extends JpaRepository<ProprietarioEndereco, Long>
 {
