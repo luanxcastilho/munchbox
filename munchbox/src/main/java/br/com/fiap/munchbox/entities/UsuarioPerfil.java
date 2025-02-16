e43f4e57983b043777e3852b30e6a683fb1407df
luanxcastilho <luan.castilho@outlook.com.br>
2025-02-16 13:34:23 -0300

    Nova API adicionada: usuarios-perfis

--- munchbox/src/main/java/br/com/fiap/munchbox/entities/UsuarioPerfil.java
+++ munchbox/src/main/java/br/com/fiap/munchbox/entities/UsuarioPerfil.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,7 +15,7 @@ import java.util.Set;
 @Setter
 @Entity
 @Table(name = "usuario_perfil")
-public class UsuarioPerfil
+public class UsuarioPerfil implements Serializable
 {
     @Serial
     private static final long serialVersionUID = 1L;
